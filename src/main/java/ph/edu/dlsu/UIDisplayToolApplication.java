47f99cd1d7c08ae1a3297a9b1efd32f0bb416820
amazingjyc <jarrell_chua@dlsu.edu.ph>
2017-11-01 15:08:01 +0800

    Renamed Class and Updated POM Description.

--- src/main/java/ph/edu/dlsu/UIDisplayToolApplication.java
+++ src/main/java/ph/edu/dlsu/UIDisplayToolApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UiParserApplication {
+public class UIDisplayToolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UiParserApplication.class, args);
+		SpringApplication.run(UIDisplayToolApplication.class, args);
 	}
 }
